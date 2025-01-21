4106b3b76d320edf4f7f10c08db8ea1dc21e9fa9
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-01-21 16:36:58 +0500

    deployed to hostinger

--- src/index.ts
+++ src/index.ts
@@ -94,6 +94,6 @@ app.get("/", (req: Request, res: Response) => {
 // });
 
 // Server
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
