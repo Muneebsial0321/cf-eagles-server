a84055e781a6faf84e34920bfa321267fc8e8336
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-01-22 14:20:36 +0500

    deployed to hostinger

--- dist/index.js
+++ dist/index.js
@@ -76,6 +76,6 @@ app.get("/", (req, res) => {
 //     }
 // });
 // Server
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,3 +1,5 @@
+yarn build
+echo "build complete"
 git add .
 git commit -m "deployed to hostinger"
 git push origin main
